1e9599f599042d361382eca45c9f0a10301af58d
sampsn <g@samp.sn>
2024-04-19 11:20:25 -0600

    Add sqlalchemy folder structure and connect to db

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,32 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-from models import Todo
+import models
+import schemas
+import crud
+
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
-todos: list[Todo] = []
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/")
-async def get_todos() -> list[Todo]:
-    return todos
+@app.get("/", response_model=List[schemas.Todo])
+async def read_todos(db: Session = Depends(get_db)):
+    return crud.get_todos(db)
 
 
-@app.post("/")
-async def create_todo(todo: Todo) -> None:
-    todos.append(todo)
+@app.post("/", response_model=schemas.Todo)
+async def make_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)) -> None:
+    crud.create_todo(db, todo)
