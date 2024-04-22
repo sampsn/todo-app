f2c3650dd6cf856a5544d6f5bae77024a7bb07a7
sampsn <g@samp.sn>
2024-04-22 09:41:14 -0600

    Create simple html frontend and a simple react front end

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import models
@@ -12,6 +13,18 @@ models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+origins = ["http://localhost:3000", "localhost:3000"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_db():
@@ -28,5 +41,5 @@ async def read_todos(db: Session = Depends(get_db)):
 
 
 @app.post("/", response_model=schemas.Todo)
-async def make_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)) -> None:
-    crud.create_todo(db, todo)
+async def make_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+    return crud.create_todo(db, todo)
